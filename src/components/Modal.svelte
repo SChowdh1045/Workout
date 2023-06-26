531bfc460151c7bfedb4aa28932675793ea36e23
Salman Chowdhury <schowdhury3262@gmail.com>
2023-06-26 01:12:09 -0400

    first commit

---
+++ src/components/Modal.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    const handleClose = () => {
+        dispatch('close');
+    }
+</script>
+
+<div style="border: 2px solid red;">
+    Something
+    <slot></slot>
+    <button on:click={handleClose}>close</button>
+</div>
